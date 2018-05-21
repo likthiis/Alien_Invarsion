5356aae78f4c9758c3dc2bfb17e8f834c3bcfdff
likthiis <1501681615@qq.com>
2018-05-21 20:12:23 +0800

    Alien_Invarsion

---
+++ alien_invasion.py
@@ -0,0 +1,50 @@
+import sys
+
+import pygame
+from pygame.sprite import Group
+
+
+from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+
+def run_game():
+	#初始化游戏并创建一个屏幕对象
+	pygame.init()
+	ai_settings = Settings() 
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+	
+	play_button = Button(ai_settings, screen, "Play")
+	
+	stats = GameStats(ai_settings)
+	sb = Scoreboard(ai_settings, screen, stats)
+	
+	#设置背景色
+	bg_color = (230,230,230)
+		
+	#创建一艘飞船
+	ship = Ship(ai_settings, screen)
+	bullets = Group()
+	aliens = Group()
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+	
+	#开始游戏的主循环
+	while True:
+		gf.check_events(ai_settings, screen, stats, sb, play_button, ship, 
+			aliens, bullets)
+		
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+			
+		gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)	
+		
+		
+run_game()
